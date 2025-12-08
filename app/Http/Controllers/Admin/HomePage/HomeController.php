a3529ffa7fffefd9fe9eca8b452e139660805239
Milad Niazi <RealMiladNiazi@gmail.com>
2025-12-08 20:39:08 +0330

    Update HomeControllers to pass reviews to views

--- app/Http/Controllers/Admin/HomePage/HomeController.php
+++ app/Http/Controllers/Admin/HomePage/HomeController.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers\Admin\HomePage;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\ReviewRepository;
 use App\Repositories\SliderRepository;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     protected SliderRepository $sliderRepo;
+    protected ReviewRepository $reviewRepo;
 
-    public function __construct(SliderRepository $sliderRepo)
+    public function __construct(SliderRepository $sliderRepo, ReviewRepository $reviewRepo)
     {
         $this->sliderRepo = $sliderRepo;
+        $this->reviewRepo = $reviewRepo;
     }
     public function index()
     {
--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -4,19 +4,25 @@ namespace App\Http\Controllers\Web;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\ReviewRepository;
 use App\Repositories\SliderRepository;
 
 class HomeController extends Controller
 {
     protected SliderRepository $sliderRepo;
+    protected ReviewRepository $reviewRepo;
 
-    public function __construct(SliderRepository $sliderRepo)
+
+    public function __construct(SliderRepository $sliderRepo, ReviewRepository $reviewRepo)
     {
         $this->sliderRepo = $sliderRepo;
+        $this->reviewRepo = $reviewRepo;
     }
     public function index()
     {
         $sliders = $this->sliderRepo->all();
-        return view('web.home.index', compact('sliders'));
+        $reviews = $this->reviewRepo->allActive();
+
+        return view('web.home.index', compact('sliders', 'reviews'));
     }
 }
