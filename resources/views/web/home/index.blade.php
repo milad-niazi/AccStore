39f9a4d8927d0c7a9d37a01f84bb1357052db8e5
Milad Niazi <RealMiladNiazi@gmail.com>
2025-12-09 17:51:50 +0330

    refactor(home): replace amazing section with categories component

--- resources/views/web/home/index.blade.php
+++ resources/views/web/home/index.blade.php
@@ -8,10 +8,10 @@
     @include('layouts.web.banner')
 
     <!-- ========== پیشنهاد شگفت انگیز ========== -->
-    @include('layouts.web.amazing')
+    @include('layouts.web.category')
 
-    <!-- ========== تخفیف دارها ========== -->
-    @include('layouts.web.offer')
+    {{-- <!-- ========== تخفیف دارها ========== -->
+    @include('layouts.web.offer') --}}
 
     <!-- ========== Customer Reviews ========== -->
     @include('layouts.web.review')
